64ac47805810d98cb5955cd8dfdf8f5ceb57ae71
U-BSD\480143 <480143@SHS-8YBF6X3.beaverton.k12.or.us>
2024-02-12 11:35:39 -0800

    Final commit

--- main.cpp
+++ main.cpp
@@ -45,7 +45,7 @@ int main(){
   fstream first_stream;
   fstream second_stream;
 
-  //opening the files
+  //opening the files and storing their results in a vector.
   first_stream.open("first.txt");
   for(int i = 0; i < 20; i++){
     char* temp = new char [15];
@@ -61,13 +61,12 @@ int main(){
     lastNames.push_back(temp);
     
   }
-  
+  //calling the randomize function, which at this point will add 400 students to the list.
   randomize(hash, firstNames, lastNames, size, 400);
 //while loop where code runs
   while (stillRunning == true){
     //prompt user to begin one of the functions
-    cout << size << endl;
-    cout << "Enter 'ADD', 'PRINT', 'DELETE', 'AVERAGE', or 'QUIT'" << endl;
+    cout << "Enter 'ADD', 'PRINT', 'DELETE', or 'QUIT'" << endl;
     char input[15];
     for(int i = 0; i < 7; i++){
       input[i] = '\0';
@@ -94,7 +93,6 @@ int main(){
       cin >> GPA;
       cin.get();
       //calling the add function
-      
       add(id, GPA, first, last, size, hash);
 
     }
@@ -110,7 +108,7 @@ int main(){
       //getting the ID to be deleted.
       cout << "What ID would you like to delete?" << endl;
 
-      //citation sophia
+      //Sophia You gave me the idea of just using my Linked List Part 2 delete function, and just setting the head to hash[index], with index being the ID modded by the size.  
       int deleteID;
       cin >> deleteID;
       cin.get();
@@ -127,7 +125,6 @@ int main(){
 void add(int Id, float GPA, char first[30], char last[30], int &size, Node** &hash){
 
   //creating the new student and setting its values
-  cout << "HI" << endl;
   student* newstudent = new student();
 
   newstudent->setId(Id);
@@ -136,28 +133,26 @@ void add(int Id, float GPA, char first[30], char last[30], int &size, Node** &ha
   
   newstudent->setGPA(GPA);
 
+  //storing the student in a node
   Node* tempNode = new Node();
   tempNode->setStudent(newstudent);
   
-
+  //finding the index that the node will go into.
   int index = newstudent->getId() % size;
-  cout << "Index: " << index << endl;
 
+  //adding the node to the hashTable.
   if(hash[index] == NULL){
-    cout << "1" << endl;
     hash[index] = tempNode;
   }
   else if(hash[index]->getNext() == NULL){
-    cout << "2" << endl;
     hash[index]->setNext(tempNode);
   }
   else if(hash[index]->getNext()->getNext()==NULL){
-    cout << "3" << endl;
     hash[index]->getNext()->setNext(tempNode);
   }
   else{
     bool rehash = true;
-    cout << "4" << endl;
+    //if there are four links, call the rehash function.
     hash[index]->getNext()->getNext()->setNext(tempNode);
     while(rehash == true){
       hashFunction(size, size*2, hash, rehash);
@@ -165,22 +160,23 @@ void add(int Id, float GPA, char first[30], char last[30], int &size, Node** &ha
 
   }
 
-
   return;
 }
 
-
+//majority of the hash function
 void hashFunction(int &size, int currentsize, Node** &hash, bool &rehash){
-
+  
   rehash = false;
+  //creating the new hash table.
   Node** newHash = new Node*[currentsize];
   for(int i = 0; i < currentsize; i++){
     newHash[i] = NULL;
 
   }
 
-
+  //for loop through every index in the hash table.
   for(int i = 0; i < size; i++){
+    //setting temporary nodes to each link in the chain.
     Node* tempNode = hash[i];
     Node* secondTempNode = NULL;
     Node* thirdTempNode = NULL;
@@ -194,44 +190,46 @@ void hashFunction(int &size, int currentsize, Node** &hash, bool &rehash){
         }
       }
     }
+    //rehashing by placing these new nodes into new indices in the new hash table.
     int index;
     if(fourthTempNode != NULL){
-      cout << "4" << endl;
       index = fourthTempNode->getStudent()->getId() % currentsize;
       rehashMechanics(fourthTempNode, hash, newHash, index, rehash);
       tempNode->getNext()->getNext()->setNext(NULL);
     }
     if(thirdTempNode != NULL){
-      cout << "3" << endl;
       index = thirdTempNode->getStudent()->getId() % currentsize;
       rehashMechanics(thirdTempNode, hash, newHash, index, rehash);
       tempNode->getNext()->setNext(NULL);
     }
     if(secondTempNode != NULL){
-      cout << "2" << endl;
       index = secondTempNode->getStudent()->getId() % currentsize;
       rehashMechanics(secondTempNode, hash, newHash, index, rehash);
       tempNode->setNext(NULL);
     }
     if(tempNode != NULL){
-      cout << "1" << endl;
       index = tempNode->getStudent()->getId() % currentsize;
       rehashMechanics(tempNode, hash, newHash, index, rehash);
     }
     
 
   }
+  //incrementing the size, deleting hash, and resetting hash.
   size = size * 2;
+  delete [] hash;
   hash = newHash;
   return;
 
 }
 
+//function where the actual adding to the newHash occurs.
 void rehashMechanics(Node* tempNode, Node ** &hash, Node ** & newHash, int index, bool &rehash){
   int iterator = 0;
+  //if the index is empty, add straight to the array.
   if(newHash[index] == NULL){
     newHash[index] = tempNode;
   }
+  //if it is full, navigate to the end of the linked list and add the node in there.
   else{
     Node* finalNode = newHash[index];
     while(finalNode->getNext() != NULL){
@@ -242,27 +240,23 @@ void rehashMechanics(Node* tempNode, Node ** &hash, Node ** & newHash, int index
       rehash = true;
     }
     finalNode->setNext(tempNode);
-    cout << finalNode->getStudent()->getId();
   }
   tempNode->setNext(NULL);
-  cout << tempNode->getStudent()->getId() << " " << index << endl;
 
 
 }
+//randomize function.
 void randomize(Node ** &hash, vector<char*> first, vector <char*> last, int &size, int numPeople){
+  //setting the seed
   srand(time(NULL));
 
   int ID = 100000;
+  //randomly generating a name and GPA, and adding to the hash table.
   for(int i = 0; i < numPeople; i++){
     int randNumFirst = rand()%20;
     int randNumSecond = rand()%20;
 
     float GPA = (float) (rand()%(100-1+1) + 1)/25;
-    cout << "First name: " <<  first.at(randNumFirst) << endl;
-    cout << "Last name: " << last.at(randNumSecond) << endl;
-    cout << "GPA: " << GPA << endl;
-    cout << "ID: " << ID << endl;
-    cout << endl;
     add(ID, GPA, first.at(randNumFirst), last.at(randNumSecond), size, hash);
     ID++;
 
@@ -312,7 +306,7 @@ void remove(Node* &head, Node* current, Node* prev, int deleteID){
 
 //display function
 void display(Node ** hash, int &size){
-  //if the current node doesn't exist, return.
+  //print out the hash table by iterating through it and printing every node in every linked list.
   for(int i = 0; i < size; i++){
     if(hash[i] != NULL){
       cout << "First name: " << hash[i]->getStudent()->first << endl;
